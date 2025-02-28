1ab697d886e4031f5cfdcd92dc564a53c77660a0
ahdsr <80558108+ahdsr@users.noreply.github.com>
2025-02-28 14:27:42 -0500

    Feat: Analytics

--- app/layout.js
+++ app/layout.js
@@ -23,6 +23,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Analytics />
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
